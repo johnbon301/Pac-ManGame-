0fac9abe877e49876e0c42ba288c447dc943e43b
johnbon301 <141462253+johnbon301@users.noreply.github.com>
2024-06-08 13:10:38 -0700

    Update game2.ino
    Completely changed a lot of stuff to make it better

--- game2.ino
+++ game2.ino
@@ -3,53 +3,37 @@
 
 
 AsyncDelay delay_1s;
+AsyncDelay delay_2s;
+AsyncDelay delay_3s;
 AsyncDelay delay_5s;
-float midi[127];
-int A_four = 440;
-bool leftButton;
-bool rightButton;
+volatile bool leftButton;
+volatile bool rightButton;
+volatile bool switchFlag;
 int leftButtonPin = 4;
 int rightButtonPin = 5;
-volatile bool switchFlag;
 int switchState = false;
 int switchPin = 7;
 volatile int positionPin = 0;
-volatile int pinPosition = 0;
+volatile int pinPosition = 6;
 volatile int timer = 0;
 volatile int points = 0;
 volatile int levelMode = 0;
 int levelComp[8] = { 0x7A1AE5, 0x5DD1FB, 0x18E7B2, 0xF3A70C, 0xFA3905, 0xF40B8D, 0xB705FA, 0x00FF6D };
-int levelDone[23][2] = {
+int levelDone[6][2] = {
   // level finished
-  { 74, 75 },   // song[0][0]], song[0][1]]
-  { 76, 75 },   // song[1][0]], song[1][1]]
-  { 77, 75 },   // song[2][0]], song[2][1]]
-  { 79, 75 },   // song[3][0]], song[3][1]]
-  { 81, 75 },   // song[4][0]], song[4][1]]
-  { 83, 200 },  // song[5][0]], song[5][1]]
-  { 81, 200 },  // song[6][0]], song[6][1]]
-  { 74, 75 },   // song[7][0]], song[7][1]]
-  { 76, 75 },   // song[8][0]], song[8][1]]
-  { 77, 75 },   // song[9][0]], song[9][1]]
-  { 79, 75 },   // song[10][0]], song[10][1]]
-  { 81, 75 },   // song[11][0]], song[11][1]]
-  { 83, 200 },  // song[12][0]], song[12][1]]
-  { 81, 200 },  // song[13][0]], song[13][1]]
-  { 76, 75 },   // song[14][0]], song[14][1]]
-  { 77, 75 },   // song[15][0]], song[15][1]]
-  { 79, 75 },   // song[16][0]], song[16][1]]
-  { 81, 75 },   // song[17][0]], song[17][1]]
-  { 83, 75 },   // song[18][0]], song[18][1]]
-  { 86, 200 },  // song[19][0]], song[19][1]]
-  { 86, 75 },   // song[20][0]], song[20][1]]
-  { 86, 75 },   // song[21][0]], song[21][1]]
-  { 86, 75 },   // song[22][0]], song[22][1]]
+  { 587, 75 },  // song[0][0]], song[0][1]]
+  { 659, 75 },  // song[1][0]], song[1][1]]
+  { 783, 75 },  // song[2][0]], song[2][1]]
+  { 880, 75 },  // song[3][0]], song[3][1]]
+  { 987, 75 },  // song[4][0]], song[4][1]]
 };
 
 void setup() {
   CircuitPlayground.begin();
   Serial.begin(9600);
   delay_1s.start(1000, AsyncDelay::MILLIS);
+  delay_2s.start(2000, AsyncDelay::MILLIS);
+  delay_3s.start(3000, AsyncDelay::MILLIS);
   delay_5s.start(5000, AsyncDelay::MILLIS);
   attachInterrupt(leftButtonPin, leftButtonISR, RISING);
   attachInterrupt(rightButtonPin, rightButtonISR, RISING);
@@ -61,44 +45,165 @@ void loop() {
   if (switchFlag) {
     delay(50);
     switchState = !switchState;
+    CircuitPlayground.clearPixels();
+    Serial.println("Game off!!");
     switchFlag = false;
   }
   if (switchState) {
-    CircuitPlayground.clearPixels();
-    timer = 0;
+    CircuitPlayground.setPixelColor(positionPin, 0xefff00);
+    if (leftButton) {
+      delay(200);
+      CircuitPlayground.setPixelColor(positionPin, 0x000000);
+      positionPin--;
+      if (positionPin < 0) {
+        positionPin = 9;
+      }
+      CircuitPlayground.setPixelColor(positionPin, 0xefff00);
+      leftButton = false;
+    }
+
+    if (rightButton) {
+      delay(200);
+      CircuitPlayground.setPixelColor(positionPin, 0x000000);
+      positionPin++;
+      if (positionPin > 9) {
+        positionPin = 0;
+      }
+      CircuitPlayground.setPixelColor(positionPin, 0xefff00);
+      rightButton = false;
+    }
+
+
+    level();
+
+    wingame();
+
+    endgame();
+  }
+}
+
+
+void ghostpink() {             // seprate from loop because it's going to run continuously on it's own,,,, will move ghost
+  if (delay_1s.isExpired()) {  //the movement of the ghost
+    Serial.println("  ");
     Serial.print("Timer:   ");
     Serial.println(timer);
-    points = 0;
+    timer++;
     Serial.print("Points:   ");
     Serial.println(points);
-    levelMode = 0;
     Serial.print("Level:   ");
     Serial.println(levelMode);
-  }
-
-  if (leftButton) {
-    delay(100);
-    positionPin--;
-    CircuitPlayground.setPixelColor(positionPin, 0xff00ef);
+    delay_1s.repeat();
+  } else if (timer == 10) {
+    delay_1s.expire();
     CircuitPlayground.clearPixels();
-    leftButton = false;
-    if (positionPin < 0) {
-      positionPin = 9;
+    points++;
+    Serial.print("Points:   ");
+    Serial.println(points);
+    timer = 0;
+    Serial.print("Level:   ");
+    Serial.println(levelMode);
+    for (int i = 0; i < sizeof(levelDone) / sizeof(levelDone[0]); i++) {  // completeing noise
+      CircuitPlayground.playTone(levelDone[i][0], levelDone[i][1]);
+      delay(1);
+      for (int l : levelComp) {
+        int randomLed = random(10);  // use for completeing level; fireworks go here
+        CircuitPlayground.setPixelColor(randomLed, l);
+        delay(10);
+        CircuitPlayground.setPixelColor(randomLed, 0x000000);
+        delay(10);
+      }
     }
-    leftButton = false;
   }
-  if (rightButton) {
-    delay(100);
-    positionPin++;
-    CircuitPlayground.setPixelColor(positionPin, 0xff00ef);
+  if (delay_5s.isExpired()) {
+    CircuitPlayground.setPixelColor(pinPosition, 0x000000);
+    pinPosition = random(10);
+    CircuitPlayground.setPixelColor(pinPosition, 0xff00ef);
+    delay_5s.repeat();
+  }
+}
+
+void ghostblue() {             // seprate from loop because it's going to run continuously on it's own,,,, will move ghost #0000ff
+  if (delay_1s.isExpired()) {  //the movement of the ghost
+    Serial.println("  ");
+    Serial.print("Timer:   ");
+    Serial.println(timer);
+    timer++;
+    Serial.print("Points:   ");
+    Serial.println(points);
+    Serial.print("Level:   ");
+    Serial.println(levelMode);
+    delay_1s.repeat();
+  } else if (timer == 10) {
+    delay_1s.expire();
     CircuitPlayground.clearPixels();
-    if (positionPin > 9) {
-      positionPin = 0;
+    points++;
+    Serial.print("Points:   ");
+    Serial.println(points);
+    timer = 0;
+    Serial.print("Level:   ");
+    Serial.println(levelMode);
+    for (int i = 0; i < sizeof(levelDone) / sizeof(levelDone[0]); i++) {  // completeing noise
+      CircuitPlayground.playTone(levelDone[i][0], levelDone[i][1]);
+      delay(1);
+      for (int l : levelComp) {
+        int randomLed = random(10);  // use for completeing level; fireworks go here
+        CircuitPlayground.setPixelColor(randomLed, l);
+        delay(10);
+        CircuitPlayground.setPixelColor(randomLed, 0x000000);
+        delay(10);
+      }
     }
-    rightButton = false;
   }
+  if (delay_3s.isExpired()) {
+    CircuitPlayground.setPixelColor(pinPosition, 0x000000);
+    pinPosition = random(10);
+    CircuitPlayground.setPixelColor(pinPosition, 0x0000ff);
+    delay_3s.repeat();
+  }
+}
 
+void ghostred() {              // seprate from loop because it's going to run continuously on it's own,,,, will move ghost   #ff0000
+  if (delay_1s.isExpired()) {  //the movement of the ghost
+    Serial.println("  ");
+    Serial.print("Timer:   ");
+    Serial.println(timer);
+    timer++;
+    Serial.print("Points:   ");
+    Serial.println(points);
+    Serial.print("Level:   ");
+    Serial.println(levelMode);
+    delay_1s.repeat();
+  } else if (timer == 10) {
+    delay_1s.expire();
+    CircuitPlayground.clearPixels();
+    points++;
+    Serial.print("Points:   ");
+    Serial.println(points);
+    timer = 0;
+    Serial.print("Level:   ");
+    Serial.println(levelMode);
+    for (int i = 0; i < sizeof(levelDone) / sizeof(levelDone[0]); i++) {  // completeing noise
+      CircuitPlayground.playTone(levelDone[i][0], levelDone[i][1]);
+      delay(1);
+      for (int l : levelComp) {
+        int randomLed = random(10);  // use for completeing level; fireworks go here
+        CircuitPlayground.setPixelColor(randomLed, l);
+        delay(10);
+        CircuitPlayground.setPixelColor(randomLed, 0x000000);
+        delay(10);
+      }
+    }
+  }
+  if (delay_2s.isExpired()) {
+    CircuitPlayground.setPixelColor(pinPosition, 0x000000);
+    pinPosition = random(10);
+    CircuitPlayground.setPixelColor(pinPosition, 0xff0000);
+    delay_2s.repeat();
+  }
+}
 
+void level() {  // change if level progression works but ghosts work
   switch (levelMode) {
     case 0:
       ghostpink();
@@ -112,202 +217,58 @@ void loop() {
     default:
       break;
   }
-
-  //endgame();
-
-  //wingame();
-}
-
-
-void ghostpink() {             // seprate from loop because it's going to run continuously on it's own,,,, will move ghost
-  if (delay_1s.isExpired()) {  //the movement of the ghost
-    Serial.print("Timer:   ");
-    Serial.println(timer);
-    timer++;
-    Serial.print("Points:   ");
-    Serial.println(points);
-    Serial.print("Level:   ");
-    Serial.println(levelMode);
-  } else if (timer == 60) {
-    delay_1s.expire();
-    points++;
-    Serial.print("Points:   ");
-    Serial.println(points);
-    timer = 0;
-    Serial.print("Level:   ");
-    Serial.println(levelMode);
-    while (delay_1s.isExpired()) {
-      int randomColor = random(sizeof(levelComp) / sizeof(int));  // use for completeing level; fireworks go here
-      int randomLed = random(10);
-      CircuitPlayground.setPixelColor(randomLed, levelComp[randomColor]);
-      delay(200);
-      CircuitPlayground.setPixelColor(randomLed, 0x000000);
-      delay(200);
-      for (int i = 0; i < sizeof(levelDone) / sizeof(levelDone[0]); i++) {  // completeing noise
-        CircuitPlayground.playTone(midi[levelDone[i][0]], levelDone[i][1]);
-        Serial.println(midi[levelDone[i][0]]);
-        delay(1);
-      }
-    }
-  }
-  if (delay_5s.isExpired()) {
-    pinPosition = random(10);
-    CircuitPlayground.setPixelColor(pinPosition, 0xff00ef);
-    //delay(2000);
-    CircuitPlayground.clearPixels();
-    //delay_5s.repeat;
-  } else if (timer == 60) {
-    delay_5s.expire();
-    CircuitPlayground.clearPixels();
-  }
-  //delay_1s.repeat;
-}
-
-void ghostblue() {             // seprate from loop because it's going to run continuously on it's own,,,, will move ghost #0000ff
-  if (delay_1s.isExpired()) {  //the movement of the ghost
-    Serial.print("Timer:   ");
-    Serial.println(timer);
-    timer++;
-    Serial.print("Points:   ");
-    Serial.println(points);
-    Serial.print("Level:   ");
-    Serial.println(levelMode);
-  } else if (timer == 60) {
-    delay_1s.expire();
-    points++;
-    Serial.print("Points:   ");
-    Serial.println(points);
-    Serial.print("Level:   ");
-    Serial.println(levelMode);
-    timer = 0;
-    while (delay_1s.isExpired()) {
-      int randomColor = random(sizeof(levelComp) / sizeof(int));  // use for completeing level; fireworks go here
-      int randomLed = random(10);
-      CircuitPlayground.setPixelColor(randomLed, levelComp[randomColor]);
-      delay(200);
-      CircuitPlayground.setPixelColor(randomLed, 0x000000);
-      delay(200);
-      for (int i = 0; i < sizeof(levelDone) / sizeof(levelDone[0]); i++) {  // completeing noise
-        CircuitPlayground.playTone(midi[levelDone[i][0]], levelDone[i][1]);
-        Serial.println(midi[levelDone[i][0]]);
-        delay(1);
-      }
-    }
-  }
-  if (delay_5s.isExpired()) {
-    pinPosition = random(10);
-    CircuitPlayground.setPixelColor(pinPosition, 0x0000ff);
-    //delay(2000);
-    CircuitPlayground.clearPixels();
-    //delay_5s.repeat;
-  } else if (timer == 60) {
-    delay_5s.expire();
-    CircuitPlayground.clearPixels();
-  }
-  //delay_1s.repeat;
-}
-
-void ghostred() {              // seprate from loop because it's going to run continuously on it's own,,,, will move ghost   #ff0000
-  if (delay_1s.isExpired()) {  //the movement of the ghost
-    Serial.print("Timer:   ");
-    Serial.println(timer);
-    timer++;
-    Serial.print("Points:   ");
-    Serial.println(points);
-    Serial.print("Level:   ");
-    Serial.println(levelMode);
-  } else if (timer == 60) {
-    delay_1s.expire();
-    points++;
-    Serial.print("Points:   ");
-    Serial.println(points); 
-    Serial.print("Level:   ");
-    Serial.println(levelMode);
-    timer = 0;
-    while (delay_1s.isExpired()) {
-      int randomColor = random(sizeof(levelComp) / sizeof(int));  // use for completeing level; fireworks go here
-      int randomLed = random(10);
-      CircuitPlayground.setPixelColor(randomLed, levelComp[randomColor]);
-      delay(200);
-      CircuitPlayground.setPixelColor(randomLed, 0x000000);
-      delay(200);
-      for (int i = 0; i < sizeof(levelDone) / sizeof(levelDone[0]); i++) {  // completeing noise
-        CircuitPlayground.playTone(midi[levelDone[i][0]], levelDone[i][1]);
-        Serial.println(midi[levelDone[i][0]]);
-        delay(1);
-      }
-    }
-  }
-  if (delay_5s.isExpired()) {
-    pinPosition = random(10);
-    CircuitPlayground.setPixelColor(pinPosition, 0xff0000);
-    //delay(2000);
-    CircuitPlayground.clearPixels();
-    //delay_5s.repeat;
-  } else if (timer == 60) {
-    delay_5s.expire();
-    CircuitPlayground.clearPixels();
-  }
-  //delay_1s.repeat;
 }
 
 
 
-void endgame() {  // determien cirtia of when the game will end
-  if (pinPosition == positionPin) {
-    CircuitPlayground.clearPixels();
-    timer = 0;  // timer resets
-    Serial.print("Timer:   ");
-    Serial.println(timer);
-    points = 0;  // points resets
-    Serial.print("Points:   ");
-    Serial.println(points);
+void wingame() {  // wingame have to check if it is working
+  if (timer == 10) {
+    levelMode++;
+  }
+  if (levelMode > 3) {
     levelMode = 0;
-    Serial.print("Level:   ");
-    Serial.println(levelMode);               // level resets
+    timer = 0;
+    points = 0;
+    Serial.println("YAY YOU WON!!!");
+  }
+}
+
+void endgame() {                     // determine cirtia of when the game will end
+  if (pinPosition == positionPin) {  // end is finshed
+    CircuitPlayground.clearPixels();
     CircuitPlayground.playTone(1500, 10);  // losing completion noise
     for (int i = 0; i < 10; i++) {         // red lights for dying??
-      CircuitPlayground.setPixelColor(i, 0xff000);
+      CircuitPlayground.setPixelColor(i, 0xff0000);
       delay(1);
     }
     for (int a = 0; a < 10; a++) {
       CircuitPlayground.setPixelColor(a, 0x000000);
       delay(1);
     }
+    Serial.println("  ");
+    timer = 0;  // timer resets
+    Serial.print("Timer:   ");
+    Serial.println(timer);
+    points = 0;  // points resets
+    Serial.print("Points:  ");
+    Serial.println(points);
+    levelMode = 0;
+    Serial.print("Level:   ");
+    Serial.println(levelMode);  // level resets
+    Serial.println("YOU LOST HAHHAHHAHA!!");
   }
 }
 
-void wingame() {
-  if (timer == 60) {
-    levelMode++;
-  }
-  if (levelMode > 2) {
-    levelMode = 0;
-  }
-  Serial.print("Points:   ");
-  Serial.println(points);
-  Serial.print("Level:   ");
-  Serial.println(levelMode);
-}
+
 
 void leftButtonISR() {
-  delay(50);
   leftButton = true;
 }
 
 void rightButtonISR() {
-  delay(50);
   rightButton = true;
 }
 
 void switchISR() {
-  delay(50);
   switchFlag = true;
-}
-
-void generateMIDI() {
-  for (int x = 0; x < 127; ++x) {
-    midi[x] = (A_four / 32.0) * pow(2.0, ((x - 9.0) / 12.0));
-    Serial.println(midi[x]);
-  }
 }
